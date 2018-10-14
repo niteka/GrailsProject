c315ce9d4119dfcffcee4f50d606aa99e0a124dc
NITEKA Aimé <nag06000@gmail.com>
2018-10-14 23:42:45 +0200

    update

--- grails-app/controllers/fr/mbds/frameworkgrails/tp1/MessageController.groovy
+++ grails-app/controllers/fr/mbds/frameworkgrails/tp1/MessageController.groovy
@@ -48,7 +48,7 @@ class MessageController {
 
         request.withFormat {
             form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'message.label', default: 'Message'), message.id])
+                //flash.message = message(code: 'default.created.message', args: [message(code: 'message.label', default: 'Message'), message.id])
                 redirect message
             }
             '*' { respond message, [status: CREATED] }
